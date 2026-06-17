7c99bcf0e5dbd575dfc9456e2f850f0410e6fd10
RossJiao <rossjiao122@gmail.com>
2026-06-17 23:24:24 +0800

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+# FizzBuzz Project - Small
+# Print numbers 1 to 100 with Fizz, Buzz, and FizzBuzz rules
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    fizzbuzz()
